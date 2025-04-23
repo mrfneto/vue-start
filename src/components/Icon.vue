1e95d195508013c4b93a38b7691adf130bbb86f2
mrfneto <mrfneto@gmail.com>
2025-04-23 00:44:54 -0300

    chore: Create vue.js project with pinia, vue-router and lucide-vue-next

---
+++ src/components/Icon.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue'
+import * as icons from 'lucide-vue-next'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  size: { type: String, default: 'md' }
+})
+
+const icon = computed(() => icons[props.name])
+</script>
+
+<template>
+  <component :is="icon" />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useStore } from '@/stores'
+import Icon from '@/components/Icon.vue'
+
+const store = useStore()
+</script>
+<template>
+  <div class="container mx-auto px-6 py-8">
+    <h1 class="flex items-center text-3xl font-bold space-x-2">
+      <Icon name="Home" />
+      <span>Home page</span>
+    </h1>
+    <p class="text-gray-700">Olá, {{ store.user?.name }}</p>
+  </div>
+</template>
